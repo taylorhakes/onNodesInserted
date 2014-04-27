b4ef524c91c3b2771d649c6ea2785f5aca6dc8bc
Taylor Hakes <taylor@taylorhakes.com>
2014-04-27 09:36:49 -0400

    Added a few more tests for node inserted

--- test/onNodesInserted.spec.js
+++ test/onNodesInserted.spec.js
@@ -21,9 +21,8 @@ describe('onNodesInserted', function() {
 				return !!els;
 			});
 			runs(function() {
-				for(var i = 0; i < els.length; i++) {
-					expect(els[i].className).toBe('hello');
-				}
+				expect(els.length).toBe(1);
+				expect(els[0].className).toBe('hello');
 			})
 		});
 		it('Add 3 elements', function() {
@@ -90,10 +89,8 @@ describe('onNodesInserted', function() {
 				return !!els;
 			});
 			runs(function() {
-				for(var i = 0; i < els.length; i++) {
-
-					expect(els[i].className).toBe('hello');
-				}
+				expect(els.length).toBe(1);
+				expect(els[0].className).toBe('hello');
 			})
 		});
 		it('Add 3 elements', function() {
@@ -137,6 +134,129 @@ describe('onNodesInserted', function() {
 			});
 			waitsFor(function() {
 				return done === 3;
+			});
+			runs(function() {
+				expect(true).toBe(true);
+			})
+		});
+	});
+	describe('Multiple elements exist and add multiple elements', function() {
+		beforeEach(function() {
+			document.innerHTML = window.__html__['test/fixtures/multiElement.html'];
+		});
+		it('Add 2 elements', function() {
+			runs(function() {
+				destroy = onNodesInserted('.hello', function(elements) {
+					els = elements;
+				});
+				setTimeout(function() {
+					document.body.innerHTML +=  window.__html__['test/fixtures/multiElement.html'];
+				}, 100);
+			});
+			waitsFor(function() {
+				return !!els;
+			});
+			runs(function() {
+				expect(els.length).toBe(2);
+				var helloCount = 0, otherHelloCount = 0;
+				for(var i = 0; i < els.length; i++) {
+					if(els[i].className.match(/hello/)) {
+						helloCount++;
+					}
+					if(els[i].className.match(/other/)) {
+						otherHelloCount++;
+					}
+				}
+				expect(helloCount).toBe(2);
+				expect(otherHelloCount).toBe(1);
+			})
+		});
+		it('Add 6 elements', function() {
+			runs(function() {
+				destroy = onNodesInserted('.hello', function(elements) {
+					els = elements;
+				});
+				setTimeout(function() {
+					document.body.innerHTML +=  window.__html__['test/fixtures/multiElement.html'];
+					document.body.innerHTML +=  window.__html__['test/fixtures/multiElement.html'];
+					document.body.innerHTML +=  window.__html__['test/fixtures/multiElement.html'];
+				}, 100);
+			});
+			waitsFor(function() {
+				return !!els;
+			});
+			runs(function() {
+				expect(els.length).toBe(6);
+				var helloCount = 0, otherHelloCount = 0;
+				for(var i = 0; i < els.length; i++) {
+					if(els[i].className.match(/hello/)) {
+						helloCount++;
+					}
+					if(els[i].className.match(/other/)) {
+						otherHelloCount++;
+					}
+				}
+				expect(helloCount).toBe(6);
+				expect(otherHelloCount).toBe(3);
+			})
+		});
+		it('Add 6 elements in succession, 2 at a time', function() {
+			var done = 0;
+			runs(function() {
+				destroy = onNodesInserted('.hello', function(elements) {
+					els = elements;
+					expect(els.length).toBe(2);
+					var helloCount = 0, otherHelloCount = 0;
+					for(var i = 0; i < els.length; i++) {
+						if(els[i].className.match(/hello/)) {
+							helloCount++;
+						}
+						if(els[i].className.match(/other/)) {
+							otherHelloCount++;
+						}
+					}
+					expect(helloCount).toBe(2);
+					expect(otherHelloCount).toBe(1);
+					done++;
+					if(done < 3) {
+						var div = document.createElement('div');
+						div.innerHTML = window.__html__['test/fixtures/multiElement.html']
+						document.body.appendChild(div);
+					}
+				});
+				setTimeout(function() {
+					document.body.innerHTML +=  window.__html__['test/fixtures/multiElement.html'];
+				}, 100);
+			});
+			waitsFor(function() {
+				return done === 3;
+			});
+			runs(function() {
+				expect(true).toBe(true);
+			})
+		});
+	});
+	describe('Multiple elements exist', function() {
+		beforeEach(function() {
+			document.innerHTML = window.__html__['test/fixtures/multiElement.html'];
+		});
+		it('Multiple elements add none', function() {
+			var called;
+			runs(function() {
+				destroy = onNodesInserted('.hello', function(elements) {
+					called = true;
+				});
+				setTimeout(function() {
+					document.body.innerHTML +=  window.__html__['test/fixtures/multiNoClass.html'];
+				}, 100);
+				setTimeout(function() {
+					if(!called) {
+						called = false;
+					}
+				}, 1000);
+			});
+			waitsFor(function() {
+				return called === false;
 			});
 			runs(function() {
 				expect(true).toBe(true);
